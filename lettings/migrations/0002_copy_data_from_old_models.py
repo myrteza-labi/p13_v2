9503966dc74f4972691fd5ce3e2e35a65fced750
myrteza-labi <myrteza@hotmail.fr>
2025-04-13 22:45:59 +0200

    Initial commit for p13_v2 with modular architecture

---
+++ lettings/migrations/0002_copy_data_from_old_models.py
@@ -0,0 +1,37 @@
+from django.db import migrations
+
+
+def copy_lettings_data(apps, schema_editor):
+    OldAddress = apps.get_model('oc_lettings_site', 'Address')
+    OldLetting = apps.get_model('oc_lettings_site', 'Letting')
+
+    NewAddress = apps.get_model('lettings', 'Address')
+    NewLetting = apps.get_model('lettings', 'Letting')
+
+    for old_address in OldAddress.objects.all():
+        new_address = NewAddress.objects.create(
+            number=old_address.number,
+            street=old_address.street,
+            city=old_address.city,
+            state=old_address.state,
+            zip_code=old_address.zip_code,
+            country_iso_code=old_address.country_iso_code
+        )
+
+        # Lier le letting à la nouvelle adresse
+        old_letting = OldLetting.objects.get(address=old_address)
+        NewLetting.objects.create(
+            title=old_letting.title,
+            address=new_address
+        )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lettings', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(copy_lettings_data),
+    ]
---
+++ lettings/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Letting
+
+def lettings_index(request):
+    lettings_list = Letting.objects.all()
+    context = {'lettings_list': lettings_list}
+    return render(request, 'lettings/index.html', context)
+
+def letting(request, letting_id):
+    letting = get_object_or_404(Letting, id=letting_id)
+    context = {
+        'title': letting.title,
+        'address': letting.address,
+    }
+    return render(request, 'lettings/letting.html', context)
