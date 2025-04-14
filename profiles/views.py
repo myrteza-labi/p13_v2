d5bb7eda748d6651699c08e2d374a6388a84f6a1
myrteza-labi <myrteza@hotmail.fr>
2025-04-14 19:54:05 +0200

    proper indent for profiles/views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Profile
 
+
 def profiles_index(request):
     profiles_list = Profile.objects.all()
     context = {'profiles_list': profiles_list}
     return render(request, 'profiles/index.html', context)
+
 
 def profile(request, username):
     profile = get_object_or_404(Profile, user__username=username)
